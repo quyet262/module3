6d41ee2e506b5257ded85ec66e4209c9d4cb559c
quyet262 <169133354+quyet262@users.noreply.github.com>
2024-07-16 10:59:30 +0700

    first commit

---
+++ bai.sql
@@ -0,0 +1,10 @@
+use demoss1;
+create table teacher(
+id int,
+name varchar(255),
+age int,
+country varchar(50)
+);
+insert into teacher (id,name,age,country)
+values (1,quyet,30,vietnam),
+        (2,nguyen,27,vietnam);
